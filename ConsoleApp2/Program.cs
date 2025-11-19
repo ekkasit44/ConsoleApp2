2ac3d0bfb719761f7dba58e91fc38b60734380dd
Ball <Ball@LAPTOP-EUEVOG8F>
2025-11-18 19:01:16 -0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ekkasit Srisuk";
+            int height = 0;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My name is Ekkasit srisuk");
+            Console.WriteLine("please, input your full name (ENG):");
+            Console.WriteLine("Computer science Program");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello Welcome"+name);
+            Console.WriteLine("Please, fill in you height(cm) : ");
+            height = Convert.ToInt16(Console.ReadLine());
+            int Weight = height - 100;
+            Console.WriteLine("You ideal weight is" + Weight.ToString());
+            Console.ReadKey();
+
+
+
+            Console.WriteLine("------------------+++++-------------------");
+            Console.Write("My Hometown is");
+            Console.Write("Udon thani");
+            Console.WriteLine("Thailand");
+
+        }
+    }
+}
